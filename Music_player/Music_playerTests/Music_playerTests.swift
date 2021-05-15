c226105c84f94dcc1121de7b727c13fe4b089d35
ar2-url <57313716+ar2-url@users.noreply.github.com>
2021-05-15 19:55:12 +0100

    Add files via upload

---
+++ Music_player/Music_playerTests/Music_playerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Music_playerTests.swift
+//  Music_playerTests
+//
+//  Created by The App Experts on 14/05/2021.
+//  Copyright © 2021 Artur Knul. All rights reserved.
+//
+
+import XCTest
+@testable import Music_player
+
+class Music_playerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
